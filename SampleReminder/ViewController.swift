2fb3c8f42df638342fd420c67f978997f300d68d
rinetrous <rinellie1228@icloud.com>
2021-05-13 15:06:00 +0900

    Initial Commit

---
+++ SampleReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleReminder
+//
+//  Created by 野田凜太郎 on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
